6aac384bb7e2a319e4bfc0fba51b168921f6547a
Kieran <kieran@betterdesigndigital.com>
2024-08-27 21:22:37 +0200

    add original create methods

--- medusajs-api/src/services/product.ts
+++ medusajs-api/src/services/product.ts
@@ -2,100 +2,115 @@ import { Lifetime } from 'awilix';
 import { ProductService as MedusaProductService, Product, User } from '@medusajs/medusa';
 import { MedusaError } from '@medusajs/utils';
 import {
-	CreateProductInput as MedusaCreateProductInput,
-	FindProductConfig,
-	ProductSelector as MedusaProductSelector,
-	UpdateProductInput as MedusaUpdateProductInput,
+    CreateProductInput as MedusaCreateProductInput,
+    FindProductConfig,
+    ProductSelector as MedusaProductSelector,
+    UpdateProductInput as MedusaUpdateProductInput,
 } from '@medusajs/medusa/dist/types/product';
 import StoreService from './store';
 import ShippingOptionRepository from '../repositories/shipping-option';
 
 type ProductSelector = {
-	store_id?: string;
+    store_id?: string;
 } & MedusaProductSelector;
 
 type CreateProductInput = {
-	store_id?: string;
-	shipping_options?: string[];
+    store_id?: string;
+    shipping_options?: string[];
 } & MedusaCreateProductInput;
 
 type UpdateProductInput = {
-	shipping_options?: string[];
+    shipping_options?: string[];
 } & MedusaUpdateProductInput;
 
 class ProductService extends MedusaProductService {
-	static LIFE_TIME = Lifetime.SCOPED;
-	protected readonly loggedInUser_: User | null;
-	protected readonly storeService_: StoreService;
-	protected readonly shippingOptionRepository_: typeof ShippingOptionRepository;
+    static LIFE_TIME = Lifetime.SCOPED;
+    protected readonly loggedInUser_: User | null;
+    protected readonly storeService_: StoreService;
+    protected readonly shippingOptionRepository_: typeof ShippingOptionRepository;
 
-	constructor(container) {
-		super(container);
+    constructor(container) {
+        super(container);
 
-		this.storeService_ = container.storeService;
-		this.shippingOptionRepository_ = container.shippingOptionRepository;
+        this.storeService_ = container.storeService;
+        this.shippingOptionRepository_ = container.shippingOptionRepository;
 
-		try {
-			this.loggedInUser_ = container.loggedInUser;
-		} catch (e) {
-			// avoid errors when backend first runs
-		}
-	}
+        try {
+            this.loggedInUser_ = container.loggedInUser;
+        } catch (e) {
+            // avoid errors when backend first runs
+        }
+    }
 
-	private prepareListConfig_(selector: ProductSelector) {
-		if (!selector.store_id && this.loggedInUser_?.store_id) {
-			selector.store_id = this.loggedInUser_.store_id;
-		}
-	}
+    private prepareListConfig_(selector: ProductSelector) {
+        if (!selector.store_id && this.loggedInUser_?.store_id) {
+            selector.store_id = this.loggedInUser_.store_id;
+        }
+    }
 
-	async list(selector: ProductSelector, config: FindProductConfig = {}): Promise<Product[]> {
-		this.prepareListConfig_(selector);
+    async list(selector: ProductSelector, config: FindProductConfig = {}): Promise<Product[]> {
+        this.prepareListConfig_(selector);
+        return await super.list(selector, config);
+    }
 
-		return await super.list(selector, config);
-	}
+    async listAndCount(selector: ProductSelector, config: FindProductConfig = {}): Promise<[Product[], number]> {
+        this.prepareListConfig_(selector);
+        return await super.listAndCount(selector, config);
+    }
 
-	async listAndCount(selector: ProductSelector, config: FindProductConfig = {}): Promise<[Product[], number]> {
-		this.prepareListConfig_(selector);
+    async retrieve(productId: string, config: FindProductConfig = {}): Promise<Product> {
+        const product = await super.retrieve(productId, config);
 
-		return await super.listAndCount(selector, config);
-	}
+        if (product.store_id && this.loggedInUser_?.store_id && product.store_id !== this.loggedInUser_.store_id) {
+            // Throw error if you don't want a product to be accessible to other stores
+            throw new MedusaError(MedusaError.Types.NOT_FOUND, 'Product does not exist');
+        }
 
-	async retrieve(productId: string, config: FindProductConfig = {}): Promise<Product> {
-		const product = await super.retrieve(productId, config);
+        return product;
+    }
 
-		if (product.store_id && this.loggedInUser_?.store_id && product.store_id !== this.loggedInUser_.store_id) {
-			throw new MedusaError(MedusaError.Types.NOT_FOUND, 'Product does not exist');
-		}
+    async create(productObject: CreateProductInput): Promise<Product> {
+        // Ensure store_id is set either from logged-in user or passed explicitly
+        if (this.loggedInUser_) {
+            productObject.store_id = this.loggedInUser_.store_id;
+        }
 
-		return product;
-	}
+        // Upsert shipping options if they exist
+        if (productObject.shipping_options?.length) {
+            productObject.shipping_options = await this.shippingOptionRepository_.upsertShippingOptions(
+                productObject.shipping_options
+            );
+        }
 
-	async createProductForStore(productObject: CreateProductInput, store_id: string): Promise<Product> {
-		const store = await this.storeService_.retrieve_(store_id);
-		if (!store) {
-			throw new MedusaError(MedusaError.Types.INVALID_DATA, 'Store not found');
-		}
+        return await super.create(productObject);
+    }
 
-		productObject.store_id = store_id;
+    async createProductForStore(productObject: CreateProductInput, store_id: string): Promise<Product> {
+        const store = await this.storeService_.retrieve_(store_id);
+        if (!store) {
+            throw new MedusaError(MedusaError.Types.INVALID_DATA, 'Store not found');
+        }
 
-		if (productObject.shipping_options?.length) {
-			productObject.shipping_options = await this.shippingOptionRepository_.upsertShippingOptions(
-				productObject.shipping_options
-			);
-		}
+        productObject.store_id = store_id;
 
-		return await super.create(productObject);
-	}
+        if (productObject.shipping_options?.length) {
+            productObject.shipping_options = await this.shippingOptionRepository_.upsertShippingOptions(
+                productObject.shipping_options
+            );
+        }
 
-	async update(productId: string, update: UpdateProductInput): Promise<Product> {
-		if (update.shipping_options) {
-			update.shipping_options = await this.shippingOptionRepository_.upsertShippingOptions(
-				update.shipping_options
-			);
-		}
+        return await super.create(productObject);
+    }
 
-		return await super.update(productId, update);
-	}
+    async update(productId: string, update: UpdateProductInput): Promise<Product> {
+        if (update.shipping_options) {
+            update.shipping_options = await this.shippingOptionRepository_.upsertShippingOptions(
+                update.shipping_options
+            );
+        }
+
+        return await super.update(productId, update);
+    }
 }
 
 export default ProductService;
